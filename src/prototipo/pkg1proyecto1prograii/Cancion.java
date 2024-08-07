98277dd3302324645075e2f89073b23e583d3655
Eduardo-45 <alaneduardodelacruz407@gmail.com>
2024-08-06 22:02:48 -0600

    primer commit

---
+++ src/prototipo/pkg1proyecto1prograii/Cancion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototipo.pkg1proyecto1prograii;
+
+/**
+ *
+ * @author trabajo
+ */
+class Cancion {
+    String titulo;
+    String artista;
+    String genero;
+
+    public Cancion(String titulo, String artista, String genero) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.genero = genero;
+        
+        
+    }
+    
+        
+    }
+    
+
