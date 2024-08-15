dd95532a2a1851c4c4d84b696cd57f7623a920f1
joshua565XD <joshua565.cruz@gmail.com>
2024-08-14 23:34:18 -0400

    Add files via upload

---
+++ SpootyApp.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spooty;
+
+/**
+ *
+ * @author alane
+ */
+import igu.FormCancion;
+
+public class SpootyApp {
+    public static void main(String[] args) {
+        FormCancion panta = new FormCancion();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+}
